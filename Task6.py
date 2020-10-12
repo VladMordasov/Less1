b2e31152d512e67165d2ea1da239eb4360ae59f8
VladMordasov <46675008+VladMordasov@users.noreply.github.com>
2020-10-13 00:27:00 +0300

    Delete Task6.py

--- Task6.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Результат спортсмена за первый день:"))
-b = float(input("Какой результат нужен: "))
-numberofdays = 1
-res = a
-while res < b:
-    res *= 1.1
-    numberofdays += 1
-print("Результат не менее " + str(b) + " будет достигнут на " + str(numberofdays) + " день.")
