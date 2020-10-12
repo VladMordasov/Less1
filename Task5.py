bae9d8003bd2f9ff461cfa5fe108118db6422fc7
VladMordasov <46675008+VladMordasov@users.noreply.github.com>
2020-10-13 00:26:45 +0300

    Delete Task5.py

--- Task5.py
+++
@@ -1,8 +0,0 @@
-Earnings = float(input("Выручка:"))
-Expences = float(input("Издержки:"))
-if Earnings > Expences:
-    print("Фирма прибыльна. Рентабельность составила " + "{:.1f}".format((Earnings - Expences) / Earnings * 100) + "%")
-    numberofemploees = int(input("Количество сотрудников фирмы: "))
-    print("Прибыль на каждого сотрудника %.1f" % ((Earnings - Expences) / numberofemploees))
-else:
-    print("Фирма уботычна.")
